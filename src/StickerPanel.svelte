1e4566c011d214e2288ca218abe36aee86b2abe9
d0kur0 <dev.dokuro@gmail.com>
2020-08-23 17:35:30 +0300

    Add sticker panel component

---
+++ src/StickerPanel.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { panelStore, rootStore } from "./globalStores";
+</script>
+
+<style>
+  @import "./styles/stickerPanel.css";
+</style>
+
+<template>
+  <div class="sticker-panel">
+    <ul class="sticker-panel__list">
+      {#each $rootStore.packs as pack}
+        <li class="sticker-panel__list-item">
+          <img src={pack.images[0]} />
+        </li>
+      {/each}
+    </ul>
+  </div>
+</template>
