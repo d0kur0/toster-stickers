c790e42011409239b0b831ed24af4c0a81eb7583
d0kur0 <dev.dokuro@gmail.com>
2020-08-23 17:35:12 +0300

    Add event handling for button

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-  import FaRegSmile from 'svelte-icons/fa/FaRegSmile.svelte'
-  export let rootForm: HTMLFormElement = undefined;
+  import FaRegSmile from "svelte-icons/fa/FaRegSmile.svelte";
+  import { panelStore } from "./globalStores";
+  import StickerPanel from "./StickerPanel.svelte";
 
-  console.log(chrome.runtime.getURL('build/packs.json'));
+  const handleOpenPanel = event => {
+    event.preventDefault();
+    const { target } = event;
+    const rects = target.getClientRects();
+    panelStore.openPanel();
+  };
 </script>
-
-<template>
-  <button class="injected-button">
-    <FaRegSmile class="" />
-  </button>
-</template>
 
 <style global>
   @import "./styles/injectedButton.css";
 </style>
+
+<template>
+  <button class="injected-button" on:click={handleOpenPanel}>
+    <FaRegSmile />
+  </button>
+</template>
