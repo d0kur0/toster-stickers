f502f0908bcbce3e6af4b1e3f5988c04130fcc12
d0kur0 <dev.dokuro@gmail.com>
2020-08-25 00:33:46 +0300

    Add observer for injectButton then open editing comment

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
-import App from "./App.svelte";
-import StickerPanel from "./StickerPanel.svelte";
+import App from "./components/App.svelte";
+import StickerPanel from "./components/StickerPanel.svelte";
 
-const forms = document.querySelectorAll(".form_answer, .form_comments");
-forms.forEach(form => {
-  const panel = form.querySelector(".icons-bar");
-  if (!panel) {
-    return;
-  }
+const injectButton = () => {
+  const forms = document.querySelectorAll(".form_answer, .form_comments");
+  forms.forEach(form => {
+    const panel = form.querySelector(".icons-bar");
+    if (!panel) {
+      return;
+    }
 
-  panel.innerHTML += `<li class="sticker-button-container"></li>`;
-  const target = panel.querySelector(".sticker-button-container");
+    if (!panel.querySelector(".sticker-button-container")) {
+      panel.innerHTML += `<li class="sticker-button-container"></li>`;
+      const target = panel.querySelector(".sticker-button-container");
 
-  target &&
-    new App({
-      target: target,
-      props: {},
-    });
-});
+      target &&
+        new App({
+          target: target,
+          props: {},
+        });
+    }
+  });
+};
+
+injectButton();
 
 const stickerPanelContainer = document.createElement("div");
 stickerPanelContainer.setAttribute("id", "sticker-panel-container");
@@ -25,4 +31,20 @@ document.body.appendChild(stickerPanelContainer);
 new StickerPanel({
   target: document.getElementById("sticker-panel-container"),
   props: {},
+});
+
+let observer = new MutationObserver(mutationRecords => {
+  mutationRecords.forEach(record => {
+    const isTextareaAdd = [...record.addedNodes].filter(
+      (node: HTMLElement) => node.className === "content-list__item",
+    );
+
+    isTextareaAdd.length && injectButton();
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+  characterDataOldValue: true,
 });
