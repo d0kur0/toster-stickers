78719ed5e46828c2f5cce05eca3488c1d48ba2c1
d0kur0 <dev.dokuro@gmail.com>
2020-08-25 00:33:17 +0300

    Rename globalStore to RootStore, add clearSelectedSticker method and add new store for LastUsedStickers

--- src/globalStores.ts
+++ src/globalStores.ts
@@ -1,14 +1,15 @@
 import { writable } from "svelte/store";
-import type { GlobalStoreStruct } from "./types/GlobalStore";
+import type { RootStore } from "./types/rootStore";
 import type { StickersCollection } from "./types/stickersCollection";
-import type { PanelStruct } from "./types/Panel";
+import type { PanelStruct } from "./types/panel";
+import type { LastUsedStickers } from "./types/lastUsedStickers";
 
 const createRootStore = () => {
   // @ts-ignore
   const getURL = window?.chrome?.runtime?.getURL;
   const packsURL = getURL ? getURL("build/packs.json") : "/build/packs.json";
 
-  const defaultStore: GlobalStoreStruct = {
+  const defaultStore: RootStore = {
     packs: [],
   };
 
@@ -43,11 +44,46 @@ const createPanelStore = () => {
     selectPack(selectedPack) {
       update(state => ({ ...state, selectedPack }));
     },
+    clearSelectPack() {
+      update(state => ({ ...state, selectedPack: undefined }));
+    },
     setOffsets({ topOffset, leftOffset }) {
       update(state => ({ ...state, leftOffset, topOffset }));
     },
   };
 };
 
+const createLastUsedStickersStore = () => {
+  const LIMIT = 12;
+  const defaultStore: LastUsedStickers = [];
+  const { subscribe, set, update } = writable(defaultStore, set => {
+    if (localStorage.lastUsedStickers === undefined) return;
+
+    try {
+      const lastUsedStickers = JSON.parse(localStorage.lastUsedStickers);
+      set(lastUsedStickers);
+    } catch (e) {
+      localStorage.lastUsedStickers = "";
+    }
+  });
+
+  return {
+    set,
+    subscribe,
+    add(image) {
+      update(store => {
+        console.log(store);
+        if (store.includes(image)) return store;
+        if (store.length > LIMIT) store.pop();
+
+        store.push(image);
+        localStorage.lastUsedStickers = JSON.stringify(store);
+        return store;
+      });
+    },
+  };
+};
+
 export const panelStore = createPanelStore();
 export const rootStore = createRootStore();
+export const lastUsedStickersStore = createLastUsedStickersStore();
