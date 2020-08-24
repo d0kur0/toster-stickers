5781e12810d2b9ee7cd2fee73b41895c60bcd821
d0kur0 <dev.dokuro@gmail.com>
2020-08-25 00:31:03 +0300

    Add paste event watching

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import FaRegSmile from "svelte-icons/fa/FaRegSmile.svelte";
-  import { panelStore, rootStore } from "./globalStores";
+  import { panelStore, rootStore } from "../globalStores";
 
   const handleOpenPanel = event => {
     event.preventDefault();
@@ -12,8 +12,6 @@
       "textarea.field__input",
     );
 
-    console.log(target?.parentElement?.parentElement?.parentElement?.parentElement);
-
     rootStore.setTextarea(textarea);
 
     if (rects && $panelStore.selfElement) {
@@ -24,11 +22,43 @@
       panelStore.openPanel();
     }
   };
+
+  const handlePaste = event => {
+    const items = event.clipboardData.items;
+
+    for (let i = 0; i < items.length; i++) {
+      console.log(items[i].type);
+      if (items[i].type.indexOf("image") === -1) continue;
+
+      const blob: Blob = items[i].getAsFile();
+      const formData = new FormData();
+      formData.append("files", blob);
+
+      fetch("https://qna.habr.com/upload?profile=fhd", {
+        headers: {
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+        },
+        referrer: window.location.href,
+        referrerPolicy: "no-referrer-when-downgrade",
+        body: formData,
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+      });
+    }
+  };
+
+  document.addEventListener("paste", handlePaste);
 </script>
 
 <style global>
-  @import "./styles/injectedButton.css";
-  @import "./styles/stickerPanel.css";
+  @import "../styles/injectedButton.css";
+  @import "../styles/stickerPanel.css";
 </style>
 
 <template>
