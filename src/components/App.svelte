160251fe9b5f63e2c073a01fd9cd7b9ec91b8859
d0kur0 <dev.dokuro@gmail.com>
2020-08-25 13:43:46 +0300

    Remove listening paste event

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -22,43 +22,12 @@
       panelStore.openPanel();
     }
   };
-
-  const handlePaste = event => {
-    const items = event.clipboardData.items;
-
-    for (let i = 0; i < items.length; i++) {
-      console.log(items[i].type);
-      if (items[i].type.indexOf("image") === -1) continue;
-
-      const blob: Blob = items[i].getAsFile();
-      const formData = new FormData();
-      formData.append("files", blob);
-
-      fetch("https://qna.habr.com/upload?profile=fhd", {
-        headers: {
-          accept: "application/json, text/javascript, */*; q=0.01",
-          "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "same-origin",
-          "x-requested-with": "XMLHttpRequest",
-        },
-        referrer: window.location.href,
-        referrerPolicy: "no-referrer-when-downgrade",
-        body: formData,
-        method: "POST",
-        mode: "cors",
-        credentials: "include",
-      });
-    }
-  };
-
-  document.addEventListener("paste", handlePaste);
 </script>
 
 <style global>
   @import "../styles/injectedButton.css";
   @import "../styles/stickerPanel.css";
+  @import "../styles/pasteImage.css";
 </style>
 
 <template>
